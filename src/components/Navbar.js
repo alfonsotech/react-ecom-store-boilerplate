825d022e77ef5c2709aaffb2de7ba7d9ade9bef9
alfonsotech <alfonsotech@nym.hush.com>
2019-04-09 01:24:41 -0400

    Adds elements to the navbar and routes to links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h3>hellow from navbar</h3>
-      </div>
+      <nav className="navbar navbar-expand-sm bg-primary  navbar-dark px-sm-5">
+        <Link to="/">Home</Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/">Products</Link>
+          </li>
+          <li className="nav-item ml-5">
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button><i className="fas fa-cart-plus" />  My Cart</button>
+        </Link>
+
+      </nav>
     )
   }
 }
